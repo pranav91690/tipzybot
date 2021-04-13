d3cd5fb0948b777809cb4583ac7c26652147a796
Pranav Achanta <pranav@Pranavs-MacBook-Pro-1192.local>
2021-04-13 21:53:22 +0530

    Updated gitignore

--- build-react-and-compose.sh
+++ build-react-and-compose.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-npm install --save --prefix=nginx-web-server/webapps/fantasywebapp/
-npm run build --prefix=nginx-web-server/webapps/fantasywebapp/
+#npm install --save --prefix=nginx-web-server/webapps/fantasywebapp/
+#npm run build --prefix=nginx-web-server/webapps/fantasywebapp/
 docker-compose up -d --build
